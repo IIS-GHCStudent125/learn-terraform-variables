8395c7147958e855d95c38c4b2a55a42d2db70de
TEK Student <TEKstudent@vlab.lan>
2026-06-04 08:04:59 -0700

    test3

--- main.tf
+++ main.tf
@@ -126,6 +126,7 @@ module "ec2_instances" {
     environment = "developmemt"
   }
 }
+
 module "s3-drs-20260604" {
   source  = "app.terraform.io/policy-as-code-training/s3-drs-20260604/aws"
   version = "1.0.0"
